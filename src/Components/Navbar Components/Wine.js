8495adc72c216ae18c21c0189b8af813d38e4c6a
Kathi <97873884+ihtaak@users.noreply.github.com>
2022-03-23 09:16:55 +0100

    Add files via upload

--- src/Components/Navbar Components/Wine.js
+++ src/Components/Navbar Components/Wine.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CharacterGrid from '../Filter/CharacterGrid';
+import Search from '../Filter/Search';
+import '../Navbar Components/Wine.css';
 
-const Wine  = () => {
-  return <div>
-    <br></br>
+const Wine = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+useEffect(() => {
+  const fetchItems = async () => {
+    setIsLoading (true)
+    const result = await axios(
+      `https://www.breakingbadapi.com/api/characters?name=${query}`
+      ) 
+
+    // console.log(result.data)
+    setItems (result.data)
+    setIsLoading(false)
+  }
+
+fetchItems ()
+}, [query])
+
+
+  return (
+    <div className="container">
+      <br></br>
     <h1 style={
       { color: "#5E2028",
       textAlign: "center"}}>Let's find the wine.</h1>
-  </div>;
-};
+  
+
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading = {isLoading} items={items}/>
+    </div>
+  );
+}
 
 export default Wine;
