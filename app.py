4bcec9610aa11fbb666ed4df275ba8e1e7132526
annoboe <97981643+annoboe@users.noreply.github.com>
2022-03-28 16:09:11 +0200

    filter_update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
-from filter import f_country, f_price, filters
+from filter import f_country, f_price, filters, price_cat, wine_cat, origin
 import pandas as pd
  
 app = Flask(__name__)
@@ -37,15 +37,50 @@ def filter():
     if request.method == "POST":
         get_filter = request.form["wine"]
         get_filter = request.form["origin"]
-        get_filter = request.form["price"]
-        get_filter = request.form["food"]
-        return redirect(url_for("get_filter", wine = get_filter, origin = get_filter, price = get_filter, food = get_filter))
+        return redirect(url_for("get_filter", wine = get_filter, origin = get_filter))
     else:
 	    return render_template("winefilter.html")
 
-@app.route('/results/<wine>/<origin>/<price>/<food>')
-def get_filter(wine, origin, price, food):
-    return filters(wine, origin, price, food)
+@app.route('/tro/<wine>/<origin>')
+def get_filter(wine, origin):
+    return filters(wine, origin)
+
+@app.route("/category", methods=["POST", "GET"])
+def category():
+    if request.method == "POST":
+        get_winecat = request.form["nm"]
+        return redirect(url_for("get_winecat", wine=get_winecat))
+    else:
+	    return render_template('wine_cat.html')
+
+@app.route("/where", methods=["POST", "GET"])
+def where():
+    if request.method == "POST":
+        get_origin = request.form["nm"]
+        return redirect(url_for("get_origin", name=get_origin))
+    else:
+	    return render_template('wine_cat.html')
+
+@app.route("/price", methods=["POST", "GET"])
+def price():
+    if request.method == "POST":
+        get_pricecat = request.form["nm"]
+        return redirect(url_for("get_pricecat", price=get_pricecat))
+    else:
+	    return render_template('price_cat.html')
+
+@app.route('/pricecat/<price>')
+def get_pricecat(price):
+    return price_cat(price)
+
+@app.route('/origin/<name>')
+def get_origin(name):
+    return origin(name)
+
+@app.route('/winecat/<wine>')
+def get_winecat(wine):
+    return wine_cat(wine)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- filter.py
+++ filter.py
@@ -3,48 +3,85 @@ import numpy as np
 
 wine_data = pd.read_csv('Data/wine_food_data_f.csv', low_memory=False)
 
+
 def f_country(country: str):
-    wine_data = pd.read_csv('data/wine_food_data_f.csv', low_memory=False)
+    wine_data = pd.read_csv('data/wine_food_data.csv', low_memory=False)
     selection_c = wine_data[wine_data['country'] == country]
     result_c = selection_c[['country', 'title', 'price', 'rating']]
     result_c_p = result_c.to_dict()
     return result_c_p
 
-def f_price(price: int):
-    wine_data = pd.read_csv('data/wine_food_data_f.csv', low_memory=False)
-    selection_p = wine_data[wine_data['price'] == price]
-    result = selection_p[['title', 'price', 'rating']]
-    result_p_p = result.to_dict()
-    return result_p_p
+# filter wine categorie
+def wine_cat(wine = 'red'):
+    cat_result = wine_data[wine_data['wine_categories'].str.contains(wine)]
+    output_dict_c = cat_result.transpose().to_dict()
+    return output_dict_c
 
-def g_rating2(rating = 3):
-    wine_data = pd.read_csv('data/wine_food_data_f.csv', low_memory=False)
-    selection_r = wine_data[wine_data['rating'] == rating]
-    result_r = selection_r[['title', 'price', 'rating']]
-    result_r = result_r.sort_values('price')
-    result_r = result_r.iloc[0:10, :]
-    result_r = result_r.to_dict()
-    return result_r
+# filter origin 
+def origin(name):
+    wine_data = pd.read_csv('data/wine_food_data.csv', low_memory=False)
+    if name == 'europe':
+        return wine_data[(wine_data['country'] == 'Spain') 
+                         | (wine_data['country'] == 'Portugal') 
+                         | (wine_data['country'] == 'France') 
+                         | (wine_data['country'] == 'Germany') 
+                         | (wine_data['country'] == 'Italy') 
+                         | (wine_data['country'] == 'Romania') 
+                         | (wine_data['country'] == 'Austria')
+                         | (wine_data['country'] == 'Slovenia')
+                         | (wine_data['country'] == 'Croatia')
+                         | (wine_data['country'] == 'England')
+                         | (wine_data['country'] == 'Czech Republic')
+                         | (wine_data['country'] == 'Bulgaria')
+                         | (wine_data['country'] == 'Greece')
+                         | (wine_data['country'] == 'Turkey')
+                         | (wine_data['country'] == 'Moldova')
+                         | (wine_data['country'] == 'Hungary')
+                         | (wine_data['country'] == 'Switzerland')
+                         | (wine_data['country'] == 'Ukraine')
+                         | (wine_data['country'] == 'Slovakia')
+                         | (wine_data['country'] == 'Serbia')
+                         | (wine_data['country'] == 'Luxembourg')
+                         | (wine_data['country'] == 'Macedonia')].transpose().to_dict()
+    elif name == 'national':
+        return wine_data[wine_data['country'] == 'Germany'].transpose().to_dict()
+    elif name == 'international':
+        return wine_data[(wine_data['country'] == 'Mexico') 
+                         | (wine_data['country'] == 'China') 
+                         | (wine_data['country'] == 'US') 
+                         | (wine_data['country'] == 'Argentina') 
+                         | (wine_data['country'] == 'Chile') 
+                         | (wine_data['country'] == 'Australia') 
+                         | (wine_data['country'] == 'South Africa') 
+                         | (wine_data['country'] == 'New Zealand') 
+                         | (wine_data['country'] == 'Israel')
+                         | (wine_data['country'] == 'Canada')
+                         | (wine_data['country'] == 'Lebanon')
+                         | (wine_data['country'] == 'Brazil')
+                         | (wine_data['country'] == 'Morocco')
+                         | (wine_data['country'] == 'Uruguay')
+                         | (wine_data['country'] == 'Peru')
+                         | (wine_data['country'] == 'India')
+                         | (wine_data['country'] == 'Georgia')
+                         | (wine_data['country'] == 'Armenia')].transpose().to_dict()
 
-def g_price(price=10):
-    wine_data = pd.read_csv('data/wine_food_data_f.csv', low_memory=False)
-    best_wine = pd.DataFrame(wine_data.groupby('price')['rating'].max())
-    return wine_data.loc[(wine_data.rating == float(best_wine.loc[price])) & (wine_data.price == price),['title', 'price', 'rating']]
 
-def g_rating(rating=3):
-    wine_data = pd.read_csv('data/wine_food_data_f.csv', low_memory=False)
-    cheapest_wine = pd.DataFrame(wine_data.groupby('rating')['price'].min())
-    return wine_data.loc[(wine_data.price == float(cheapest_wine.loc[rating])) & (wine_data.rating == rating),['title', 'price', 'rating']]
+# filter price category 
+def price_cat(price = 1):
+    selection = wine_data[wine_data['price_cat'] == price]
+    result_p = selection.sort_values('price')
+    result_p = result_p.transpose().to_dict()
+    return result_p
+
 
 
 # filter together
-def filters(wine, origin, price, food):
-    wine_data = pd.read_csv('data/wine_food_data.csv', low_memory=False)
+def filters(wine, origin):
     if wine in ['red', 'white', 'rosé']:
         wine_cat = wine_data[wine_data['wine_categories'].str.contains(wine)]
-    elif wine == 0: 
+    else: 
         wine_cat = wine_data
-        
+       
     if origin == 'europe':
         wine_o = wine_cat[(wine_cat['country'] == 'Spain') 
                          | (wine_cat['country'] == 'Portugal') 
@@ -89,16 +126,8 @@ def filters(wine, origin, price, food):
                          | (wine_cat['country'] == 'India')
                          | (wine_cat['country'] == 'Georgia')
                          | (wine_cat['country'] == 'Armenia')]
-    elif origin == 0:
+    else:
         wine_o = wine_cat
-        
-    if price in [1, 2, 3]:
-        wine_p = wine_o[wine_o['price_cat'] == price]
-    elif price == 0:
-        wine_p = wine_o
     
-    if food == 1:
-        wine_f = wine_p
-    elif food == 0:
-        wine_f = wine_p.drop('food', axis = 1)
-    return wine_f
+    output = wine_o.transpose().to_dict()
+    return output
